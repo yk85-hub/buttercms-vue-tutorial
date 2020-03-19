5811f02dbeaea51b8c44992f8e892cf922f24666
yk85-hub <55225896+yk85-hub@users.noreply.github.com>
2020-03-19 21:07:26 +0300

    Update CustomersHome.vue

--- src/components/CustomersHome.vue
+++ src/components/CustomersHome.vue
@@ -18,13 +18,7 @@
                 <div class="media-content">
                   <div class="content">
                     <h2 class="title is-5">{{ page.fields.headline }}</h2>
-                    new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue.js!'
-    }
-})
-                    
+     dgfdgfgdgdfg               
                   </div>
                 </div>
               </article>
